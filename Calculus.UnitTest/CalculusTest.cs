8fecfd6893488202e1383e8e4b794377e39ee5ad
s1am13 <92172011+PolGarciaBlanch@users.noreply.github.com>
2022-05-10 17:09:20 +0200

    joder loco

--- Calculus.UnitTest/CalculusTest.cs
+++ Calculus.UnitTest/CalculusTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NUnit.Framework;
+using MathCalculus;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,12 @@ namespace Calculus.UnitTest
 {
     public class CalculusTest
     {
+        [TestCase(0, 5)]
+        public void SetDeCalcul_Infinite_ExpectedException(int a, int b)
+        {
+            var rules = new Calculus();
+            var result = rules.SetDeCalcul(a, b);
+            Assert.That(result, Throws.Div);
+}
     }
 }
